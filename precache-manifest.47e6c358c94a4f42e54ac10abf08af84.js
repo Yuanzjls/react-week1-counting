92f19c88b8d3a3a462cacddda1e8e4fb19acc7d1
Yuan Zhou <yzhou@windsormachine.com>
2021-03-05 08:39:14 -0500

    Updates

---
+++ precache-manifest.47e6c358c94a4f42e54ac10abf08af84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0b9a0dd4c2924b332f8767eb04c763f",
+    "url": "/react-week1-counting/index.html"
+  },
+  {
+    "revision": "b722b3d3a678bec20dc1",
+    "url": "/react-week1-counting/static/css/main.6da786cc.chunk.css"
+  },
+  {
+    "revision": "a01976bc5dae5a963c11",
+    "url": "/react-week1-counting/static/js/2.f101725a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-week1-counting/static/js/2.f101725a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b722b3d3a678bec20dc1",
+    "url": "/react-week1-counting/static/js/main.a6a8717a.chunk.js"
+  },
+  {
+    "revision": "ff0f9654007d8d6a6f94",
+    "url": "/react-week1-counting/static/js/runtime-main.b49d4d43.js"
+  }
+]);
