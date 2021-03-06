6fc7a5c258e240575011b22c15d44577be56c8d3
Yuan Zhou <scottzhouls@gmail.com>
2021-03-06 10:37:30 -0500

    add jss feature

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,12 +1,23 @@
 import React, {useState, useEffect} from "react";
+import {createUseStyles} from 'react-jss'
+
+const useStyles = createUseStyles({
+  myButton:{
+    color:'black',
+    background:'white',
+    "&:focus":{      
+        background:'gray',      
+    }
+  }
+});
 
 function Button(props){
-
+    const classes = useStyles();
     const className = (props.btnType!="reset")?"btn toggle-btn":"";
 
     return (
         <button type="button"
-          className={className} disabled={props.btnEnable} onClick={props.btnFunc} accessKey={props.btnKey}>
+          className={classes.myButton} disabled={props.btnEnable} onClick={props.btnFunc} accessKey={props.btnKey}>
         <span>{props.btnText}</span>
         </button>
     );
